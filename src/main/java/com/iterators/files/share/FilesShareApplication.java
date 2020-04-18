8f2654f6091305540014f24d25d34066ebb0712f
iterators <iterators@ushareit.com>
2020-04-18 18:54:51 +0800

    :art: init repo

---
+++ src/main/java/com/iterators/files/share/FilesShareApplication.java
@@ -0,0 +1,13 @@
+package com.iterators.files.share;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilesShareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilesShareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iterators/files/share/FilesShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iterators.files.share;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesShareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
